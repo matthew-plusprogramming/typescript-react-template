83d0ca0dbd733a073aaee45d2da0c79b9cedc9c4
Matthew Lin <matthewlinplusprogramming@gmail.com>
2022-05-08 11:20:06 -0700

    :lipstick: Add classnames package

--- src/components/TemplateComponent.tsx
+++ src/components/TemplateComponent.tsx
@@ -1,24 +1,18 @@
+import classname from 'classnames';
 import { motion } from 'framer-motion';
 import React, { HTMLProps, useState } from 'react';
-
-// Style imports
-
-// Page imports
-
-// Component imports
-
-// Context imports
 
 interface TemplateComponentProps extends HTMLProps<HTMLDivElement> {
   testProp: string;
 }
 
 const TemplateComponent: React.FC<TemplateComponentProps> = ({
-  testProp
+  testProp,
+  className
 }) => {
   const [ testState, setTestState ] = useState<string>('');
   return (
-    <motion.div>
+    <motion.div className={classname(className)}>
 
     </motion.div>
   );
